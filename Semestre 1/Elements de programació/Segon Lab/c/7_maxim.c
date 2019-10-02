be3f3c85d05e30032c5e17b93f7565660157954b
Joanp131 <Joanpaucm28@gmail.com>
2019-10-02 23:49:42 +0200

    Maxim.c does not work

--- Semestre 1/Elements de programació/Segon Lab/c/7_maxim.c
+++ Semestre 1/Elements de programació/Segon Lab/c/7_maxim.c
@@ -3,32 +3,39 @@
 
 int main(void) {
 
-    bool sort = true;
-    int i, l;
+    int i, l, sort = 1;
 
+    /*
     printf("De quants nombres vols trobar el màxim?\n");
     scanf("%d", &l);
-
-    float n[l], max;
-
+    */
+   
+    l = 3;
+    int n[3];
+ 
+    /*
     printf("Dona'm tres nombres reals: \n");
+    scanf("%f %f %f", &n[0], &n[1], &n[2]);
+    */
+
     for (i = 0; i < l; i++) {
-        scanf("%f", &n[i]);
+        printf("Dona'm un nombre enter: \n");
+        scanf("%d", &n[i]);
     }
 
     while (sort) {
-        sort = false;
+        sort = 0;
         for (i = 0; i < l-1; l++) {
             float prev = n[i];
             if (prev > n[i+1]) {
                 n[i] = n[i+1];
                 n[i+1] = prev;
-                sort = true;
+                sort = 1;
             }
         }
     }
 
-    printf("El màxim dels %d nombres és %f", l, n[l-1]);
+    printf("El màxim dels %d nombres és %d", l, n[l-1]);
     
     return 0;
 }
