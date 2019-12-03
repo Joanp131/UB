f111517fe094983c6779487ec958ed044ad29d38
Joanp131 <Joanpaucm28@gmail.com>
2019-12-03 23:11:01 +0100

    Llista 4

--- Semestre 1/Elements de programació/Llista 4/c/62_angleVectors.c
+++ Semestre 1/Elements de programació/Llista 4/c/62_angleVectors.c
@@ -3,10 +3,15 @@
 #include <stdio.h>
 #include <math.h>
 
+#define PI 3.14159265359
+
 double prodEscalar(double[], double[], int);
+double Vlen(double[], int);
 
 int main(void) {
     int n, i;
+    double cosin;
+
     printf("Quina vols que sigui la dimensió dels vectors?\n");
     scanf("%d", &n);
 
@@ -21,4 +26,37 @@ int main(void) {
         scanf("%lg", &y[i]);
     }
 
+    printf("Pr es -> %lg\n", prodEscalar(x, y, n));
+    printf("Len x -> %lg\n", Vlen(x, n));
+    printf("Len y -> %lg\n", Vlen(y, n));
+
+    cosin = prodEscalar(x, y, n);
+    cosin /=  (double) (Vlen(x, n) * Vlen(y, n));
+    printf("Cosin -> %lg\n", cosin);
+
+    printf("L'angle entre els vectors és: %lg\n", acos(cosin)*180./PI);
+
+    return 0;
+}
+
+double prodEscalar(double x[], double y[], int dim) {
+    int i;
+    double sum = 0;
+
+    for(i = 0; i < dim; i++) {
+        sum += x[i]*y[i];
+    }
+
+    return sum;
+}
+
+double Vlen(double x[], int dim) {
+    double sum;
+    int i;
+
+    for(i = 0; i < dim; i++) {
+        sum += x[i]*x[i];
+    }
+
+    return sqrt(sum);
 }
